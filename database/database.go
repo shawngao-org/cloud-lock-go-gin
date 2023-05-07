0dada5eacea648e13277f9ec6fdbcd8446e55972
ShawnGao <2519306859@qq.com>
2023-05-07 18:22:15 +0800

    Modify: Change mysql package to GORM.

--- database/database.go
+++ database/database.go
@@ -3,29 +3,26 @@ package database
 import (
 	"cloud-lock-go-gin/config"
 	"cloud-lock-go-gin/logger"
-	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"os"
 )
 
-var Conn = connectDb()
+var Db = connectDb()
 
-func connectDb() *sql.DB {
+func connectDb() *gorm.DB {
 	host := config.Conf.Database.Mysql.Host
 	port := config.Conf.Database.Mysql.Port
 	user := config.Conf.Database.Mysql.User
 	pwd := config.Conf.Database.Mysql.Password
 	db := config.Conf.Database.Mysql.Db
 	logger.LogInfo("[MYSQL] Connecting to mysql server " + host + ":" + port + "...")
-	dsn, err := sql.Open("mysql", user+":"+pwd+"@tcp("+host+":"+port+")/"+db)
+	uri := user + ":" + pwd + "@tcp(" + host + ":" + port + ")/" + db
+	dsn, err := gorm.Open(mysql.Open(uri), &gorm.Config{})
 	if err != nil {
 		logger.LogErr("[MYSQL] Connection to mysql server " + host + ":" + port + " -----> FAILED")
 		logger.LogErr("%s", err)
-		err := dsn.Close()
-		if err != nil {
-			logger.LogErr("[MYSQL] Close mysql connect " + host + ":" + port + " -----> FAILED")
-			logger.LogErr("%s", err)
-		}
 		os.Exit(-1)
 	}
 	logger.LogSuccess("[MYSQL] Connection to mysql server " + host + ":" + port + " -----> SUCCESS")
--- go.mod
+++ go.mod
@@ -20,6 +20,8 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -35,4 +37,6 @@ require (
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gorm.io/driver/mysql v1.5.0 // indirect
+	gorm.io/gorm v1.25.0 // indirect
 )
