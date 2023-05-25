855aea164bbc1d5c240f7f1188172ecb1fe776bd
ShawnGao <2519306859@qq.com>
2023-05-25 20:20:24 +0800

    New: Add server handle.

---
+++ server/server.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"cloud-lock-go-gin/config"
+	"cloud-lock-go-gin/database"
+	"cloud-lock-go-gin/logger"
+	"cloud-lock-go-gin/middleware"
+	"cloud-lock-go-gin/redis"
+	"cloud-lock-go-gin/router"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/net/context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var pack = "main"
+
+func ServerHandle() {
+	r := gin.Default()
+	r.Use(middleware.RequestMiddleware())
+	r.Use(middleware.ResponseMiddleware())
+	//r.Use(middleware.AuthMiddleware())
+	router.LoadRouter(r)
+	srv := &http.Server{
+		Addr:    config.Conf.Server.Ip + ":" + config.Conf.Server.Port,
+		Handler: r,
+	}
+	go func() {
+		startServer(srv)
+	}()
+	shutdownServer(srv)
+}
+
+func startServer(srv *http.Server) {
+	logger.LogInfo(pack, "Starting server...")
+	logger.LogSuccess(pack, "Listening address -----> %s", srv.Addr)
+	err := srv.ListenAndServe()
+	if err != nil {
+		logger.LogWarn(pack, "%s", err)
+	}
+}
+
+func shutdownServer(srv *http.Server) {
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	logger.LogWarn(pack, "Shutdown server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		logger.LogWarn(pack, "%s", err)
+	}
+	logger.LogSuccess(pack, "Exited -----> SUCCESS")
+}
+
+func Connect() {
+	database.ConnectDb()
+	redis.RedisConnect()
+}
