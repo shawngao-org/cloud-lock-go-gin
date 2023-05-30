7c33ee1dd4f26545f2ac4a9f1869f588ce5b21a7
ShawnGao <2519306859@qq.com>
2023-05-30 19:58:12 +0800

    New: Add authentication of tokens in authentication middleware.

--- middleware/request_middleware.go
+++ middleware/request_middleware.go
@@ -1,7 +1,10 @@
 package middleware
 
 import (
+	"cloud-lock-go-gin/jwt"
+	"cloud-lock-go-gin/logger"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func RequestMiddleware() gin.HandlerFunc {
@@ -14,7 +17,27 @@ func RequestMiddleware() gin.HandlerFunc {
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//user, err := getCurrentUser(c)
+		token := c.Request.Header.Get("token")
+		jt, err := jwt.ParseToken(token)
+		if err != nil {
+			logger.LogErr("AuthMiddleware", "%s", err)
+			c.AbortWithStatus(http.StatusForbidden)
+			c.JSON(http.StatusForbidden, gin.H{
+				"message":   "Invalid token format.",
+				"exception": err.Error(),
+			})
+			return
+		}
+		err = jt.Claims.Valid()
+		if err != nil {
+			logger.LogErr("AuthMiddleware", "%s", err)
+			c.AbortWithStatus(http.StatusForbidden)
+			c.JSON(http.StatusForbidden, gin.H{
+				"message":   "Invalid token.",
+				"exception": err.Error(),
+			})
+			return
+		}
 		c.Next()
 	}
 }
