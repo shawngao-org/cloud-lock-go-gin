dec2575aae061ac598e4b268add9d5afe7a92a96
ShawnGao <2519306859@qq.com>
2023-06-06 15:15:41 +0800

    :card_file_box: Add device table.

---
+++ database/device.go
@@ -0,0 +1,8 @@
+package database
+
+type Device struct {
+	Id    string `gorm:"primaryKey;column:id;size:36;not null;comment:Device id;"`
+	Name  string `gorm:"not null;column:name;default:未命名设备;size:32;comment:Device name;"`
+	Addr  string `gorm:"not null;column:addr;size:100;comment:Device address;"`
+	Model string `gorm:"not null;column:model;size:50;comment:Device model;"`
+}
