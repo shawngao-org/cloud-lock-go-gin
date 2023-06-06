dd409e664a1e7e00dc4e77318348337bf85da97f
ShawnGao <2519306859@qq.com>
2023-06-06 17:22:05 +0800

    :sparkles: Add influxdb connect.

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"cloud-lock-go-gin/config"
 	"cloud-lock-go-gin/database"
+	"cloud-lock-go-gin/influxdb"
 	"cloud-lock-go-gin/redis"
 	"cloud-lock-go-gin/server"
 )
 
 func main() {
 	config.GetConfig()
+	influxdb.ConnectInfluxDb()
 	database.ConnectDb()
 	redis.RedisConnect()
 	server.ServerHandle()
