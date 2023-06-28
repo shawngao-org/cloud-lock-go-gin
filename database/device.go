bbfd0ec790ecf7d3fecc25d5cb238b6028aea1a5
ShawnGao <2519306859@qq.com>
2023-06-28 19:06:27 +0800

    :card_file_box: The SN code field is added to the device table.

--- database/device.go
+++ database/device.go
@@ -5,4 +5,5 @@ type Device struct {
 	Name  string `gorm:"not null;column:name;default:未命名设备;size:32;comment:Device name;"`
 	Addr  string `gorm:"not null;column:addr;size:100;comment:Device address;"`
 	Model string `gorm:"not null;column:model;size:50;comment:Device model;"`
+	Sn    string `gorm:"uniqueKey;not null;column:sn;size:36;comment:SN;"`
 }
