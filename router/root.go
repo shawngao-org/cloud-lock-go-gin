abf58e5b13928a3edeb9b659b6baf0581dad15c6
ShawnGao <2519306859@qq.com>
2023-06-29 17:15:05 +0800

    :art: Change ping router to public router.

--- router/root.go
+++ router/root.go
@@ -2,14 +2,10 @@ package router
 
 import (
 	"cloud-lock-go-gin/controller"
-	"cloud-lock-go-gin/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func RootRouter(r *gin.Engine) {
-	authorized := r.Group("/", middleware.AuthMiddleware())
-	{
-		authorized.GET("/ping", controller.Ping)
-	}
 	r.POST("/login", controller.Login)
+	r.GET("/ping", controller.Ping)
 }
