e3b07d1bd756e6bf55e4db1bce0c40efe5665c35
ShawnGao <2519306859@qq.com>
2023-07-27 01:00:36 +0800

    :card_file_box: Added a unique index to the permissions field of the routing table.

--- database/router.go
+++ database/router.go
@@ -9,7 +9,7 @@ type Router struct {
 	Id         int64  `gorm:"primaryKey;column:id;autoIncrement;not null;"`
 	Path       string `gorm:"uniqueKey;column:path;not null;size:255;"`
 	Method     string `gorm:"not null;column:method;size:10;"`
-	Permission string `gorm:"not null;column:permission;size:64;"`
+	Permission string `gorm:"uniqueKey;not null;column:permission;size:64;"`
 }
 
 func CheckRouterPermission(path string, uid int64) (bool, error) {
